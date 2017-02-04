961fb85d1a26ee6e6375bdd39348f23221502131
Rollin Francois <Rollin@rollins-macbook-pro.local>
2017-02-04 12:51:53 -0500

    To create a subclass of UIStackView / To override the initializers

---
+++ FoodTracker8.0/RatingControl.swift
@@ -0,0 +1,23 @@
+//
+//  RatingControl.swift
+//  FoodTracker8.0
+//
+//  Created by Rollin Francois on 2/4/17.
+//  Copyright © 2017 Francois Technology. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    //MARK: Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+}
