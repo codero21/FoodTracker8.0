38d66184d57e1fdcb941568019134b1bd254cc61
Rollin Francois <Rollin@rollins-macbook-pro.local>
2017-02-04 02:12:26 -0500

    To implement the label reset action in the ViewController code

--- FoodTracker8.0/ViewController.swift
+++ FoodTracker8.0/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
 
     //MARK: Actions
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        
+        mealNameLabel.text = "Set Default Text! ✔"
     }
     
 }
