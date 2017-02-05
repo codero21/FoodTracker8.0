7849137b3281a05cb8769ad2f4ba1e8c8c50ee6b
Rollin Francois <Rollin@rollins-macbook-pro.local>
2017-02-04 21:28:41 -0500

    To add inspectable properties

--- FoodTracker8.0/RatingControl.swift
+++ FoodTracker8.0/RatingControl.swift
@@ -16,6 +16,19 @@ import UIKit
     
     var rating = 0
     
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+    
+
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
+    
 
     //MARK: Initialization
     
@@ -43,7 +56,14 @@ import UIKit
     
     private func setupButtons() {
         
-        for _ in 0..<5 {
+        // clear any existing buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
+        for _ in 0..<starCount {
             // Create the button
             let button = UIButton()
             
@@ -51,8 +71,8 @@ import UIKit
             
             // Add constraints
             button.translatesAutoresizingMaskIntoConstraints = false
-            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
             
             // Setup the button action
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
