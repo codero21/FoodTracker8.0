5866fb12eadc3c9a8b0d5bd144929ed69b6d20d5
Rollin Francois <Rollin@rollins-macbook-pro.local>
2017-02-05 11:27:11 -0500

    To implement the rating action

--- FoodTracker8.0/RatingControl.swift
+++ FoodTracker8.0/RatingControl.swift
@@ -14,7 +14,11 @@ import UIKit
     
     private var ratingButtons = [UIButton]()
     
-    var rating = 0
+    var rating = 0 {
+        didSet {
+            updateButtonSelectionStates()
+        }
+    }
     
     @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
         didSet {
@@ -46,10 +50,26 @@ import UIKit
     
     
     //MARK: Button Action
+        
+        func ratingButtonTapped(button: UIButton) {
+            guard let index = ratingButtons.index(of: button) else {
+                fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+            }
+            
+            // Calculate the rating of the selected button
+            let selectedRating = index + 1
+            
+            if selectedRating == rating {
+                // If the selected star represents the current rating, reset the rating to 0.
+                rating = 0
+            } else {
+                // Otherwise set the rating to the selected star
+                rating = selectedRating
+            }
+            
+            print("Button Pressed 👍🏾")
+        }
     
-    func ratingButtonTapped(button: UIButton) {
-        print("Button Pressed 👍🏾")
-    }
     
     
     //MARK: Private Methods
@@ -74,7 +94,7 @@ import UIKit
             self.traitCollection)
         
         
-        for _ in 0..<starCount {
+        for index in 0..<starCount {
             // Create the button
             let button = UIButton()
             
@@ -101,4 +121,10 @@ import UIKit
         }
     }
 
+    private func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerated() {
+            // If the index of a button is less than the rating, that button should be selected.
+            button.isSelected = index < rating
+        }
+    }
 }
